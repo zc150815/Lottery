2ccf43b25bf03db91594948c84e9440cfdfdbc4e
zc150815 <983943921@qq.com>
2016-07-24 15:20:11 +0800

    lottery

---
+++ 网易彩票(new)/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  网易彩票(new)
+//
+//  Created by Ning Xie on 16/6/20.
+//  Copyright © 2016年 itcast. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
